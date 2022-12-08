552528dc29b50e2c0745659669ae6e4f5b88a2ab
Emre <emreceyhan98@hotmail.com>
2022-12-08 18:14:27 +0000

    Factory method based on emplee departments

---
+++ Week4Project/src/main/java/com/sparta/EmployeeFactory.java
@@ -0,0 +1,30 @@
+package com.sparta;
+
+public class EmployeeFactory {
+    public Employee createEmployee(String channel, int emp_no, String birth_date, String first_name, String last_name, char gender, String hire_date)
+    {
+        switch(channel)
+        {
+            case "Customer Service":
+                return new CustomerService(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Human Resources":
+                return new HumanResources(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Marketing":
+                return new Marketing(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Development":
+                return new Development(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Finance":
+                return new Finance(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Quality Management":
+                return new QualityManagement(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Research":
+                return new Research(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Sales":
+                return new Sales(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            case "Production":
+                return new Production(emp_no, birth_date, first_name, last_name, gender, hire_date);
+            default:
+                throw new IllegalArgumentException("Unknown channel "+channel);
+        }
+    }
+}
