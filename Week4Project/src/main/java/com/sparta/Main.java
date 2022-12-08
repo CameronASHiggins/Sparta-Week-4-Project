9e32431d6bff710580f5cf9c16aaf5351a2086b0
JON-BRANDON LEMARD-REID <1800575@chester.ac.uk>
2022-12-08 18:20:48 +0000

    Added employee constructor and 2 format drivers

--- Week4Project/src/main/java/com/sparta/Main.java
+++ Week4Project/src/main/java/com/sparta/Main.java
@@ -1,7 +1,13 @@
 package com.sparta;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        try{
+//            // convert obj to format
+//        } catch (JsonProcessingException e){
+//            throw new RuntimeException(e);
+//        }
     }
 }
