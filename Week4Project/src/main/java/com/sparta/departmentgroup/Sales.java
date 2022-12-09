459790a543ab2391e40327b18f97c7e6c8b6307d
JON-BRANDON LEMARD-REID <1800575@chester.ac.uk>
2022-12-09 11:07:23 +0000

    Added LocalDate for the employee class and subclasses Added package for employee subclasses

---
+++ Week4Project/src/main/java/com/sparta/departmentgroup/Sales.java
@@ -0,0 +1,19 @@
+package com.sparta.departmentgroup;
+
+import com.sparta.Department;
+import com.sparta.Employee;
+
+import java.time.LocalDate;
+
+public class Sales extends Employee implements Department {
+
+
+    public Sales(int emp_no, LocalDate birth_date, String first_name, String last_name, char gender, LocalDate hire_date) {
+        super(emp_no, birth_date, first_name, last_name, gender, hire_date);
+    }
+
+    @Override
+    public void job() {
+        System.out.println("I am part of Sales");
+    }
+}
