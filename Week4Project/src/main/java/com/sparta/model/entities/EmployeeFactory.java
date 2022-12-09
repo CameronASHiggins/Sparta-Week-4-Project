a1027ecefd122782560b272c935cd9c2e2ad280f
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2022-12-09 17:05:19 +0000

    Changed the EmployeeFactory to better represent the table we're reading

--- Week4Project/src/main/java/com/sparta/model/entities/EmployeeFactory.java
+++ Week4Project/src/main/java/com/sparta/model/entities/EmployeeFactory.java
@@ -5,30 +5,30 @@ import com.sparta.*;
 
 
     public class EmployeeFactory {
-        public Employee createEmployee(String channel, int emp_no, String birth_date, String first_name, String last_name, String gender, String hire_date)
+        public Employee createEmployee(String departmentNo, int emp_no, String birth_date, String first_name, String last_name, String gender, String hire_date)
         {
-            switch(channel)
+            switch(departmentNo)
             {
-                case "Customer Service":
+                case "d009":
                     return new CustomerService(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Human Resources":
+                case "d003":
                     return new HumanResources(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Marketing":
+                case "d001":
                     return new Marketing(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Development":
+                case "d005":
                     return new Development(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Finance":
+                case "d002":
                     return new Finance(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Quality Management":
+                case "d006":
                     return new QualityManagement(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Research":
+                case "d008":
                     return new Research(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Sales":
+                case "d007":
                     return new Sales(emp_no, birth_date, first_name, last_name, gender, hire_date);
-                case "Production":
+                case "d004":
                     return new Production(emp_no, birth_date, first_name, last_name, gender, hire_date);
                 default:
-                    throw new IllegalArgumentException("Unknown channel "+channel);
+                    throw new IllegalArgumentException("Unknown channel "+departmentNo);
             }
         }
     }
